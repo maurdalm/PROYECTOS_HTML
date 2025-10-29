f67069e3fadbacd8b71c6ceb29eb04e671d44805
maurdalm <maurdalm@gmail.com>
2025-10-28 21:06:26 -0500

    cambios curso 28/10/2025

---
+++ EJERCICIO 08/script.js
@@ -0,0 +1,8 @@
+//script.js
+const miboton = document.getElementById('miboton');
+
+miboton.addEventListener('click', function() {
+    //Cambiar el color de fondo y el color de texto
+    miboton.style.backgroundColor = 'blue';
+    miboton.style.color = 'white';
+});
